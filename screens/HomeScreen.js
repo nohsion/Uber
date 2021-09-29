0f3917c80ec100f8bb7f9c87fb0d06f90fa25bb3
Sion Noh <shtldhs123@ajou.ac.kr>
2021-09-29 23:23:34 +0900

    초기 설정 (redux, tailwind classnames)

---
+++ screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import tw from 'tailwind-react-native-classnames';
+
+const HomeScreen = () => {
+    return (
+        <SafeAreaView>
+            <Text style={tw`text-red-500 p-10`}>I am the HomeScreen</Text>
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+    text: {
+        color: "blue",
+    },
+})
