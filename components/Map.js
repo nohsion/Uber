9553bc16d5ef966ab8a4f0f907b56e18ccf6579d
Sion Noh <shtldhs123@ajou.ac.kr>
2021-09-30 10:59:31 +0900

    구글 길찾기 (대한민국 서비스 x)

--- components/Map.js
+++ components/Map.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import MapView, { Marker } from 'react-native-maps';
+import MapViewDirections from 'react-native-maps-directions';
 import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
-import { selectOrigin } from '../slices/navSlice';
+import { selectDestination, selectOrigin } from '../slices/navSlice';
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const Map = () => {
     const origin = useSelector(selectOrigin)
-
+    const destination = useSelector(selectDestination)
+    if (origin && destination) {
+        console.log(origin)
+        console.log(destination)
+    }
     return (
         <MapView
             style={tw`flex-1`}
@@ -19,6 +25,19 @@ const Map = () => {
                 longitudeDelta: 0.0421,
             }}
         >
+            {origin && destination && (
+                <MapViewDirections
+                    origin={origin.description}
+                    destination={destination.description}
+                    apikey={GOOGLE_MAPS_APIKEY}
+                    strokeWidth={3}
+                    strokeColor="hotpink"
+                    onError={(errorMessage) => {
+                        console.log('GOT AN ERROR: ', errorMessage)
+                    }}
+                />
+            )}
+
             {origin?.location && (
                 <Marker
                     coordinate={{
