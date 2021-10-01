5e49a1065b3a5e4e7d5060a0e2f3588bf7bdbd3d
Sion Noh <shtldhs123@ajou.ac.kr>
2021-10-01 18:35:11 +0900

    NavFavorites 즐겨찾기 (집, 직장)

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames';
 import NavOptions from '../components/NavOptions';
+import NavFavorites from '../components/NavFavorites';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import { GOOGLE_MAPS_APIKEY } from "@env"
 import { useDispatch } from 'react-redux';
@@ -58,6 +59,7 @@ const HomeScreen = () => {
                 />
 
                 <NavOptions />
+                <NavFavorites />
             </View>
         </SafeAreaView>
     )
